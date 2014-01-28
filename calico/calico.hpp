3158cb6f538aa0380c0a10f0b23d7a59e6f4fdad
Phil Ruffwind <rf@rufflewind.com>
2014-01-28 00:29:56 -0500

    Fix punctuation error.

--- calico/calico.hpp
+++ calico/calico.hpp
@@ -11,7 +11,7 @@ The only exception is the [Windows entry point module](@ref winentry.h), which
 must be compiled and linked with the rest of the program.
 
 Start by examining the various [modules](modules.html) and
-[headers](files.html):
+[headers](files.html).
 
 */
 #error "This is a dummy header file and must not be included."
