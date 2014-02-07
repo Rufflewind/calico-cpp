1bcff10bd241c5521319ae25b4586014697236f8
Phil Ruffwind <rf@rufflewind.com>
2014-02-07 01:11:44 -0500

    Added scaling_lens (+ minor bugfix for negating_lens)

--- calico/lens.hpp
+++ calico/lens.hpp
@@ -152,7 +152,7 @@ struct lens_base {
 /// ensure better optimization for floating-point numbers (when `-ffast-math`
 /// or its equivalent is not enabled).
 template<class Lens, class T = typename lens_traits<Lens>::value_type>
-struct negating_lens : lens_base<negating_lens<Lens>, T> {
+struct negating_lens : lens_base<negating_lens<Lens, T>, T> {
 
     /// The value type accepted by the lens.
     typedef T value_type;
@@ -204,6 +204,68 @@ private:
 /// Constructs a `negating_lens` from a given value.
 template<class T> inline negating_lens<T> make_negating_lens(T&& x) {
     return negating_lens<T>(std::forward<T>(x));
+}
+
+/// A lens that scales the value by a factor before storing or accessing it.
+///
+/// If `L` is the `scaling_lens`, `x` is the internal lens, and `k` is the
+/// factor to be multiplied, the following relationships are satisfied:
+///
+///     L       ==    x / k
+///     L = c  <==>   x = c * k
+///
+template<class Lens, class Factor,
+         class T = typename lens_traits<Lens>::value_type>
+struct scaling_lens : lens_base<scaling_lens<Lens, Factor, T>, T> {
+
+    /// The value type accepted by the lens.
+    typedef T value_type;
+
+    /// The type of the factor to be multiplied.
+    typedef Factor factor_type;
+
+    /// The type of the lens being wrapped.
+    typedef Lens internal_type;
+
+    /// Wraps a lens.
+    scaling_lens(internal_type&& data, factor_type&& factor)
+        : _data(std::forward<internal_type>(data)),
+          _factor(std::forward<factor_type>(factor)) {}
+
+    /// Returns the internal lens.
+    internal_type data() { return _data; };
+
+    /// Returns the internal lens.
+    const internal_type& data() const { return _data; };
+
+    /// Returns the factor to be multiplied.
+    const factor_type& factor() const { return _factor; };
+
+    /// Gets the value through the lens.
+    operator value_type() const
+    { return static_cast<value_type>(data()) / _factor; }
+
+    /// Puts a value through the lens.
+    scaling_lens& operator=(const value_type& x) {
+        data() = x * _factor;
+        return *this;
+    }
+
+    /// Puts a value through the lens (rvalue version).
+    scaling_lens& operator=(value_type&& x) {
+        data() = std::move(x) * _factor;
+        return *this;
+    }
+
+private:
+    factor_type _factor;
+    internal_type _data;
+};
+
+/// Constructs a `scaling_lens` from a given value.
+template<class T, class U> inline
+scaling_lens<T, U> make_scaling_lens(T&& lens, U&& factor) {
+    return scaling_lens<T, U>(std::forward<T>(lens), std::forward<U>(factor));
 }
 
 }
